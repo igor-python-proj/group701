0d785b37ef25d24e857245bd68697f845b2b9f90
Igor <igorg789170@gmail.com>
2026-09-11 16:19:50 +0500

    Добавил пример методов класса

---
+++ lesson_4_2.py
@@ -0,0 +1,31 @@
+# переменные класса и виды методов
+
+class Visitor:
+    # переменные класса
+    organisation = "Geeks"
+    count = 0
+
+    def __init__(self, name):
+        if not self.validate_name(name):
+            raise ValueError("Имя не должно быть пустым")
+        self.name = name
+        # self.count = 0 - не работает так как об-ы не умеют получать доступ к другим об-м
+        Visitor.count += 1
+
+    @classmethod
+    def get_visitor_count(cls):
+        return cls.count
+
+    @staticmethod
+    def validate_name(name):
+        if name.strip():
+            return True
+        return False
+
+
+visitor1 = Visitor("Игорь")
+visitor2 = Visitor("Алмаз")
+Visitor.organisation = "Об.Ц. Geeks"
+print(Visitor.organisation, Visitor.count)
+print(Visitor.get_visitor_count())
+print(Visitor.validate_name("       "))
